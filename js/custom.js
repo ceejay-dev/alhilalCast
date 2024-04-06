28de02646d2956ea1766ce1aead508ddf8009dee
ceejay-dev <candidojoao12@gmail.com>
2024-04-06 01:07:44 +0100

    botao de legenda funcionando

--- js/custom.js
+++ js/custom.js
@@ -1,38 +1,23 @@
 const video = document.querySelector('#meuvideo');
-  // Botão de controle de mudo
+  // evento para ativar ou desativar o som
   const toggleMuteButton = document.getElementById('toggleMute');
   toggleMuteButton.addEventListener('click', function() {
       if (video.muted) {
           video.muted = false;
-          toggleMuteButton.innerText = 'Mudo';
+          //toggleMuteButton.innerText = 'Mudo';
       } else {
           video.muted = true;
-          toggleMuteButton.innerText = 'Som';
+          //toggleMuteButton.innerText = 'Som';
       }
 
  
   });
 
-  /*document.addEventListener('DOMContentLoaded', function() {
-    const videoFrame = document.getElementById('meuvideo');
-    const togglePauseButton = document.getElementById('togglePause');
-
-    togglePauseButton.addEventListener('click', function() {
-        const video = videoFrame.contentWindow.document.querySelector('iframe');
-        if (video.paused) {
-            video.play();
-            togglePauseButton.innerText = 'Pause';
-        } else {
-            video.pause();
-            togglePauseButton.innerText = 'Play';
-        }
-    });
-});*/
 
 video = document.getElementById('meuvideo');
 const togglePauseButton = document.getElementById('togglePause');
 
-
+//função para pausar e continuar o vídeo
 function pausar(){
     if (video.paused) {
         video.play();
@@ -43,14 +28,26 @@ function pausar(){
     }
 }
 
-  // Script customizado para controlar ações dos botões
-document.addEventListener('DOMContentLoaded', function() {
-      // Botão de controle de legenda
-    const toggleSubtitlesButton = document.getElementById('toggleSubtitles');
-    toggleSubtitlesButton.addEventListener('click', function() {
-        console.log('Botão de legenda clicado');
-        // Abre o modal de importação de legendas quando o botão é clicado
-        var myModal = new bootstrap.Modal(document.getElementById('legendModal'));
-        myModal.show();
-    });
-  });
+//função para ativar e desativar a legenda
+function toggleSubtitles() {
+    var video = document.getElementById('meuvideo');
+    var track = document.querySelector('track');
+
+    if (track) {
+        if (track.track.mode === 'showing') {
+            track.track.mode = 'hidden';
+        } else {
+            track.track.mode = 'showing';
+        }
+    } else {
+        var newTrack = document.createElement('track');
+        newTrack.src = 'content/legendas/The Pursuit Of Happyness 2006 BrRip x264.vtt';
+        newTrack.kind = 'subtitles';
+        newTrack.srclang = 'en';
+        newTrack.label = 'English';
+        newTrack.default = true;
+        video.appendChild(newTrack);
+    }
+}
+
+
